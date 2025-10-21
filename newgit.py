6e498d3fe7d903a70e190eecd555ed15a91eec26
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2025-10-21 20:30:06 +0300

    pra

--- newgit.py
+++ newgit.py
@@ -4,3 +4,12 @@ def check(n):
     else:
         print(False)
 check(90)
+# inheritace of the class form to other 
+class Class:
+    var = 24  #class variable 
+    def __init__(self,name ,where):
+        self.where = where #instance variable 
+    def check(self,name ,nan):
+        self.name = name 
+        self.nan = nan
+
